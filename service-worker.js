42ac23d720d8a46d9b37b3354e43e15595054e1b
visu250328 <viktor.sundvall@gmail.com>
2025-03-28 12:10:30 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,7 +9,6 @@ self.addEventListener('install', function(event) {
     })
   );
 });
-
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
